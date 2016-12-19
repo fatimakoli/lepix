5ddb4fea2ec481e08612d8472e6859a23751f86b
gabriellet <gat2118@columbia.edu>
2016-12-19 08:57:01 -0500

    cleanup demo dir

--- demo/images.sh
+++ demo/images.sh
@@ -10,46 +10,47 @@ LLI="lli"
 #!/bin/sh
 
 ../source/lepix.native <greyscale.lepix> greyscale.ll
-lli greyscale.ll > greyscale.ppm
+lli greyscale.ll > output/greyscale.ppm
 echo "greyscale finished"
 
 ../source/lepix.native <filter-green.lepix> green.ll
-lli green.ll > green.ppm
+lli green.ll > output/green.ppm
 echo "filter green finished"
 
 ../source/lepix.native <filter-blue.lepix> blue.ll
-lli blue.ll > blue.ppm
+lli blue.ll > output/blue.ppm
 echo "filter blue finished"
 
 ../source/lepix.native <filter-red.lepix> red.ll
-lli red.ll > red.ppm
+lli red.ll > output/red.ppm
 echo "filter red finished"
 
 ../source/lepix.native <remove-green.lepix> green.ll
-lli green.ll > green2.ppm
+lli green.ll > output/green2.ppm
 echo "remove green finished"
 
 ../source/lepix.native <remove-blue.lepix> blue.ll
-lli blue.ll > blue2.ppm
+lli blue.ll > output/blue2.ppm
 echo "remove blue finished"
 
 ../source/lepix.native <remove-red.lepix> red.ll
-lli red.ll > red2.ppm
+lli red.ll > output/red2.ppm
 echo "remove red finished"
 
 ../source/lepix.native <neg.lepix> neg.ll
-lli neg.ll > neg.ppm
+lli neg.ll > output/neg.ppm
 echo "negative finished"
 
 ../source/lepix.native <flip.lepix> flip.ll
-lli flip.ll > flip.ppm
+lli flip.ll > output/flip.ppm
 echo "flip finished"
 
 ../source/lepix.native <blur.lepix> blur.ll
-lli blur.ll > blur.ppm
+lli blur.ll > output/blur.ppm
 echo "flip finished"
 
 ../source/lepix.native <extremecontrast.lepix> extremecontrast.ll
-lli extremecontrast.ll > extremecontrast.ppm
+lli extremecontrast.ll > output/extremecontrast.ppm
 echo "extreme contrast finished"
 
+rm *.ll
