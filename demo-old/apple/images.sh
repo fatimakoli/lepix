50767d38f08a65def297415552121c9d6d6a538e
Gabrielle <gat2118@columbia.edu>
2016-12-20 00:16:08 -0500

    file cleanup

---
+++ demo-old/apple/images.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir output
+files="*.lepix"
+
+
+for file in $files
+do
+    basename=`echo $file | sed 's/.*\\///
+                             s/.lepix//'`
+    ../../source/lepix.native <$file> $basename.ll
+	lli $basename.ll > output/$basename.ppm
+	echo "$basename finished"
+done
+
+rm *.ll
